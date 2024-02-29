8f28236f8dfbdbb2301ea9c35abc65001fc1885a
pawara101 <pawara101dassanayake@gmail.com>
2024-02-29 16:40:54 +0530

    First-dash-app | v1.1

--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
 # Import packages
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 
@@ -10,21 +10,22 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 app = Dash(__name__)
 
 # App layout
-# app.layout = html.Div([
-#     html.Div(children='My First App with Data'),
-#     dash_table.DataTable(data=df.to_dict('records'), page_size=10)
-# ])
-
-
-# App layout
 app.layout = html.Div([
-    html.Div(children='Dash App'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=15),
-    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg')),
-    dcc.Graph(figure=px.line(df, x='country', y='gdpPercap')),
-    dcc.Graph(figure=px.histogram(df, x='continent', y='gdpPercap', histfunc='avg'))
+    html.Div(children='My First App with Data, Graph, and Controls'),
+    html.Hr(),
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+    dcc.Graph(figure={}, id='controls-and-graph')
 ])
 
+# Add controls to build the interaction
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
 
 # Run the app
 if __name__ == '__main__':
