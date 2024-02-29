9151e6cfcf0ed77f6545c329dbb0c570bd4fc024
pawara101 <pawara101dassanayake@gmail.com>
2024-02-29 21:36:44 +0530

    First-dash-app | v1.9

--- test2.py
+++ test2.py
@@ -8,19 +8,20 @@ app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(options=[{'label': country, 'value': country} for country in df['country'].unique()],
+                 value='Canada', id='dropdown-selection'),
+    dcc.Dropdown(options=[{'label': cat, 'value': cat} for cat in ['pop', 'lifeExp', 'gdpPercap']],
+                 id='dropdown-selection-cat', value='pop'),
     dcc.Graph(id='graph-content')
 ])
 
-@callback(
+@app.callback(
     Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    [Input('dropdown-selection', 'value'), Input('dropdown-selection-cat', 'value')]
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+def update_graph(value, cat):
+    dff = df[df['country'] == value]
+    return px.line(dff, x='year', y=cat)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
