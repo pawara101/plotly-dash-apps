e8ad0a05fb87a903d1939a46415ca91008af1fd9
pawara101 <pawara101dassanayake@gmail.com>
2024-02-29 19:36:25 +0530

    First-dash-app | v1.5

--- components.py
+++ components.py
@@ -1,11 +1,19 @@
-from dash import Dash,dcc,html
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 
-# dcc.Checklist(
-#     ['New York City', 'Montréal', 'San Francisco'],
-#     ['New York City', 'Montréal'],
-#     inline=True
-# )
+
 
 app = Dash(__name__)
 
-app.layout =  html.Div()
+app.layout =  html.Div([
+html.H1("Hello"),
+
+dcc.Checklist(
+    ['New York City', 'Montréal', 'San Francisco'],
+    ['New York City', 'Montréal'],
+    inline=True
+)
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
