dfda2ba65ca8849a98ed1c1d94e4b53991441f98
pawara101 <pawara101dassanayake@gmail.com>
2024-03-05 19:58:45 +0530

    Plotly Pie chart 1.1

--- plotly_pie.py
+++ plotly_pie.py
@@ -2,22 +2,32 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 
 app = Dash(__name__)
-df = px.data.tips() # replace with your own data source
+
 
 app.layout = html.Div([
     html.H4('Analysis of the restaurant sales'),
-    dcc.Graph(id="graph",figure=px.pie(df, values="total_bill", names="smoker", hole=.3)),
+    dcc.Graph(id="graph"),
+    html.P("Names:"),
+    dcc.Dropdown(id='names',
+        options=['smoker', 'day', 'time', 'sex'],
+        value='day', clearable=False
+    ),
+    html.P("Values:"),
+    dcc.Dropdown(id='values',
+        options=['total_bill', 'tip', 'size'],
+        value='total_bill', clearable=False
+    ),
 ])
 
 
-# @app.callback(
-#     Output("graph", "figure"), 
-#     Input("names", "value"), 
-#     Input("values", "value"))
-# def generate_chart(names, values):
-#     df = px.data.tips() # replace with your own data source
-#     fig = px.pie(df, values=values, names=names, hole=.3)
-#     return fig
+@app.callback(
+    Output("graph", "figure"), 
+    Input("names", "value"), 
+    Input("values", "value"))
+def generate_chart(names, values):
+    df = px.data.tips() # replace with your own data source
+    fig = px.pie(df, values=values, names=names, hole=.3)
+    return fig
 
 
 app.run_server(debug=True)
